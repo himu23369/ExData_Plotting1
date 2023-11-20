22caf3bf6f6e1f231bf9c912b4566cb0966c896d
Himanshu <hbansal_be21@thapar.edu>
2023-11-20 17:36:12 +0530

    Improvements

--- plot3.R
+++ plot3.R
@@ -1,13 +1,14 @@
 # Reading, naming and subsetting power consumption data
+getwd()
 power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subSetData <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+datetime <- strptime(paste(subpower$Date, subpower$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subpower$Global_active_power)
+subMetering1 <- as.numeric(subpower$Sub_metering_1)
+subMetering2 <- as.numeric(subpower$Sub_metering_2)
+subMetering3 <- as.numeric(subpower$Sub_metering_3)
 
 png("plot3.png", width=480, height=480)
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
--- plot4.R
+++ plot4.R
@@ -1,16 +1,17 @@
 # Reading, naming and subsetting power consumption data
 power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
-subSetData <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
+subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
-datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-globalActivePower <- as.numeric(subSetData$Global_active_power)
-globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
-voltage <- as.numeric(subSetData$Voltage)
-subMetering1 <- as.numeric(subSetData$Sub_metering_1)
-subMetering2 <- as.numeric(subSetData$Sub_metering_2)
-subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+datetime <- strptime(paste(subpower$Date, subpower$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subpower$Global_active_power)
+globalReactivePower <- as.numeric(subpower$Global_reactive_power)
+voltage <- as.numeric(subpower$Voltage)
+subMetering1 <- as.numeric(subpower$Sub_metering_1)
+subMetering2 <- as.numeric(subpower$Sub_metering_2)
+subMetering3 <- as.numeric(subpower$Sub_metering_3)
 
+# Setting the height and width of plot image
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
 
